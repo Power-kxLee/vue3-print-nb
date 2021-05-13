efc0632ac445b04e0b18c62c0adc9a066ffb572c
ex_likx5 <ex_likx5@partner.midea.com>
2021-05-13 09:23:40 +0800

    打印预览和打印url地址

--- src/print/packages/print.js
+++ src/print/packages/print.js
@@ -2,7 +2,7 @@
  * @Author: lee
  * @Date: 2021-05-10 11:45:50
  * @LastEditors: lee
- * @LastEditTime: 2021-05-11 14:17:22
+ * @LastEditTime: 2021-05-13 09:21:52
  * @Description: file content
  */
 import Print from './printarea.js';
@@ -23,48 +23,55 @@ const addEvent = (element, type, callback) => {
 export default {
   directiveName: 'print',
   mounted (el, binding, vnode) {
-    console.log(el, binding.instance.pringLoading)
-    let closeBtn = true;
+    let vue = binding.instance;
     let id = '';
-    const localPrint = () => {
-      if (closeBtn) {
-        closeBtn = false;
-        new Print({
-          ids: id, // * 局部打印必传入id
-          standard: '', // 文档类型，默认是html5，可选 html5，loose，strict
-          extraHead: binding.value.extraHead, // 附加在head标签上的额外标签,使用逗号分隔
-          extraCss: binding.value.extraCss, // 额外的css连接，多个逗号分开
-          popTitle: binding.value.popTitle, // title的标题
-          openCallback () { // 调用打印之后的回调事件
-            binding.value.openCallback && binding.value.openCallback(binding.instance)
-            closeBtn = true;
-          },
-          closeCallback () {
-            binding.value.closeCallback && binding.value.closeCallback(binding.instance)
-          },
-          beforeOpenCallback () {
-            binding.value.beforeOpenCallback && binding.value.beforeOpenCallback(binding.instance)
-          }
-        });
-      }
-    };
+
     addEvent(el, 'click', () => {
       if (typeof binding.value === 'string') {
+        // 全局打印
         id = binding.value;
+        window.print();
+        return
       } else if (typeof binding.value === 'object' && !!binding.value.id) {
+        // 局部打印
         id = binding.value.id;
         let ids = id.replace(new RegExp("#", "g"), '');
         let elsdom = document.getElementById(ids);
         if (!elsdom) console.log("id in Error"), id = '';
+
       }
-      // 局部打印
-      if (id) {
-        localPrint();
-      } else {
-        // 直接全局打印
-        window.print();
-      }
+      localPrint();
     })
+    const localPrint = () => {
+      new Print({
+        ids: id, // * 局部打印必传入id
+        url: binding.value.url, // 打印指定的网址，这里不能跟id共存 如果共存id的优先级会比较高
+        standard: '', // 文档类型，默认是html5，可选 html5，loose，strict
+        extraHead: binding.value.extraHead, // 附加在head标签上的额外标签,使用逗号分隔
+        extraCss: binding.value.extraCss, // 额外的css连接，多个逗号分开
+        previewTitle: binding.value.previewTitle || '打印预览', // 打印预览的标题
+        previewPrintBtnLabel: binding.value.previewPrintBtnLabel || '打印', // 打印预览的标题
+        popTitle: binding.value.popTitle, // title的标题
+        preview: binding.value.preview || false, // 是否启动预览模式
+        asyncUrl: binding.value.asyncUrl,
+        previewBeforeOpenCallback () { // 预览窗口打开之前的callback
+          binding.value.previewBeforeOpenCallback && binding.value.previewBeforeOpenCallback(vue)
+        },
+        previewOpenCallback () { // 预览窗口打开之后的callback
+          binding.value.previewOpenCallback && binding.value.previewOpenCallback(vue)
+        },
+        openCallback () { // 调用打印之后的回调事件
+          binding.value.openCallback && binding.value.openCallback(vue)
+        },
+        closeCallback () {
+          binding.value.closeCallback && binding.value.closeCallback(vue)
+        },
+        beforeOpenCallback () {
+          binding.value.beforeOpenCallback && binding.value.beforeOpenCallback(vue)
+        }
+      });
+
+    };
   }
 
 };
